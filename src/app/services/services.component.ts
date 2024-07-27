120ba8c90ff02c975db73e4a6146ee71639d3055
Bara87 <bara-2010@hotmail.fr>
2024-07-27 17:21:35 +0200

    services-component

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArtisanDataService, Artisan } from '../artisan-data.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+  artisans: Artisan[] = [];
 
+  constructor(private artisanDataService: ArtisanDataService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.artisans = this.artisanDataService.getArtisansByCategory('Services');
+  }
+
+  selectArtisan(artisan: Artisan): void {
+    console.log('Navigating to artisan detail with ID:', artisan.id);
+    this.artisanDataService.setCurrentArtisanId(artisan.id);
+    this.router.navigate(['/artisan-detail']);
+  }
 }
