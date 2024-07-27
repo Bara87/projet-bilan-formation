6fee4b72202d1f420c410e532fafa71bdcdb5f93
Bara87 <bara-2010@hotmail.fr>
2024-07-27 17:57:14 +0200

    artisan-detail

--- src/app/artisan-detail/artisan-detail.component.ts
+++ src/app/artisan-detail/artisan-detail.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtisanDataService, Artisan } from '../artisan-data.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-artisan-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
+  selector: 'app-artisan-detail',
   templateUrl: './artisan-detail.component.html',
-  styleUrl: './artisan-detail.component.scss'
+  styleUrls: ['./artisan-detail.component.scss']
 })
-export class ArtisanDetailComponent {
+export class ArtisanDetailComponent implements OnInit {
+  artisan: Artisan | undefined;
 
-}
+  name = '';
+  subject = '';
+  message = '';
+  formSubmitted = false; 
+
+  constructor(
+    private router: Router,
+    private artisanDataService: ArtisanDataService
+  ) {}
+
+  ngOnInit(): void {
+    this.artisan = this.artisanDataService.getCurrentArtisan();
+    if (this.artisan) {
+      console.log('Artisan found:', this.artisan);
+    } else {
+      console.log('No artisan found.');
+    }
+  }
+
+  submitForm() {
+    this.formSubmitted = true;
+
+    if (!this.artisan) return;
+
+    if (!this.name || !this.subject || !this.message) {
+      return;
+    }
+
+    const formData = {
+      artisanId: this.artisan.id,
+      name: this.name,
+      subject: this.subject,
+      message: this.message
+    };
+
+    console.log('Form data to submit:', formData);
+    this.resetForm();  
+  }
+
+  resetForm() {
+    this.name = '';
+    this.subject = '';
+    this.message = '';
+    this.formSubmitted = false;
+  }
+	}
+
+
