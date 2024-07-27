9c978200907806539401c50f868752ae93beaf67
Bara87 <bara-2010@hotmail.fr>
2024-07-27 15:43:45 +0200

    footer-component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent {}
 
-}
