f686809ee54edb83dd99eaadb29cf483fb7d1860
Bara87 <bara-2010@hotmail.fr>
2024-07-27 17:34:11 +0200

    alimentation-component

--- src/app/alimentation/alimentation.component.ts
+++ src/app/alimentation/alimentation.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArtisanDataService, Artisan } from '../artisan-data.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-alimentation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './alimentation.component.html',
-  styleUrl: './alimentation.component.scss'
+  styleUrls: ['./alimentation.component.scss']
 })
-export class AlimentationComponent {
+export class AlimentationComponent implements OnInit {
+  artisans: Artisan[] = [];
 
+  constructor(private artisanDataService: ArtisanDataService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.artisans = this.artisanDataService.getArtisansByCategory('Alimentation');
+  }
+
+  selectArtisan(artisan: Artisan): void {
+    console.log('Navigating to artisan detail with ID:', artisan.id);
+    this.artisanDataService.setCurrentArtisanId(artisan.id);
+    this.router.navigate(['/artisan-detail']);
+  }
 }
