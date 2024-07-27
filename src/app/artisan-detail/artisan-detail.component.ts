3a411f6a58cc5324862e9973a14ad1a220d24f8a
Bara87 <bara-2010@hotmail.fr>
2024-07-27 15:12:06 +0200

    components

---
+++ src/app/artisan-detail/artisan-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisan-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisan-detail.component.html',
+  styleUrl: './artisan-detail.component.scss'
+})
+export class ArtisanDetailComponent {
+
+}
