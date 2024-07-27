5a16420d447559b0f2df0269ef1886d47a5a93fb
Bara87 <145752545+Bara87@users.noreply.github.com>
2024-07-27 18:23:26 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { ArtisanDetailComponent } from './artisan-detail/artisan-detail.component';
+import { ArtisanListComponent } from './artisan-list/artisan-list.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'artisans', component: ArtisanListComponent },
+  { path: 'batiment', component: BatimentComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'fabrication', component: FabricationComponent },
+  { path: 'alimentation', component: AlimentationComponent },
+  
+  { path: 'artisan-detail', component: ArtisanDetailComponent },
+  { path: '**', component: NotFoundComponent }
+];
