eec5a4410b2d2b2df2183de494a4bad0160812d8
Bara87 <bara-2010@hotmail.fr>
2024-07-29 13:34:52 +0200

    meta elements introduction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { ArtisanDataService, Artisan } from '../artisan-data.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
@@ -13,12 +14,22 @@ import { ArtisanDataService, Artisan } from '../artisan-data.service';
 export class HomeComponent implements OnInit {
   artisans: Artisan[] = [];
   topArtisans: Artisan[] = [];
-  
 
-  constructor(private artisanDataService: ArtisanDataService, private router: Router) {}
+  constructor(
+    private artisanDataService: ArtisanDataService,
+    private router: Router,
+    private titleService: Title,
+    private metaService: Meta
+  ) {}
 
   ngOnInit(): void {
     this.topArtisans = this.artisanDataService.getTopArtisans();
+
+    this.titleService.setTitle('Trouve ton Artisan - Accueil');
+    this.metaService.addTags([
+      { name: 'description', content: 'Trouvez le meilleur artisan près de chez vous avec Trouve ton Artisan. Comparez les avis et choisissez le professionnel qui répond à vos besoins.' },
+      { name: 'keywords', content: 'artisans, trouver, comparer, avis, professionnels, services' }
+    ]);
   }
 
   selectArtisan(artisan: Artisan): void {
